f00e9676deb7161cb100db44f5272c8b3023d9a1
Brian O'Meara <omeara.brian@gmail.com>
2018-06-11 23:49:49 -0400

    stub of new approach

---
+++ R/core.R
@@ -0,0 +1,12 @@
+# Recoding in a simpler way
+
+#' Create set of model conditions
+#'
+#' Takes the data and returns all possible models for faMuSSE
+#'
+#' @param traits data.frame: binary traits with row.names for taxa
+#' @return a data.frame of possible models
+fm_models <- function(traits) {
+  nchar <- ncol(traits)
+  
+}
